41ac1c3c447d76d64937f1070ac7bc418c9e0633
hadi <tayebi1426@gmail.com>
2022-08-22 21:48:24 +0430

    init

--- src/main/java/org/cmp/k8s/api/PodController.java
+++ src/main/java/org/cmp/k8s/api/PodController.java
@@ -1,8 +1,17 @@
 package org.cmp.k8s.api;
 
+import org.cmp.k8s.model.http.HttpResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pods")
 public class PodController {
 
+    @GetMapping
+    public ResponseEntity<HttpResponseBody<?>> list(){
+        return null;
+    }
 }
---
+++ src/main/java/org/cmp/k8s/model/dto/PodDTO.java
@@ -0,0 +1,7 @@
+package org.cmp.k8s.model.dto;
+
+public class PodDTO {
+    private String uid;
+    private String node;
+    private String status;
+}
